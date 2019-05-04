79a716fcc0483f368241f441e9bf33612af9dff7
James Lin <b04901069@ntu.edu.tw>
2019-05-04 17:54:28 +0800

    save mean_std

--- final/analysis.py
+++ final/analysis.py
@@ -6,16 +6,21 @@ x_train_path = '../../data_CSIE_ML/X_train/arr_0.npy'
 y_train_path = '../../data_CSIE_ML/Y_train/arr_0.npy'
 x_test_path  = '../../data_CSIE_ML/X_test/arr_0.npy'
 
-def save_mean_std(path, name):
-    data = np.load(path)
+def save_mean_std():
+    data_train = np.load(x_train_path)
+    data_test  = np.load(x_test_path)
+    data = np.vstack([data_train, data_test])
+    del data_train
+    del data_test
+    print(data.shape)
     mean_std = np.zeros((2, data.shape[1]))
     mean_std[0] = np.mean(data, 0)
     mean_std[1] = np.std(data, 0)
     print(mean_std.shape)
-    print('mean | std:')
-    for i in range(mean_std.shape[1]) :
-        print('{}: {} | {}'.format(i, mean_std[0, i], mean_std[1, i]))
-    np.save(name, mean_std)
+    # print('mean | std:')
+    # for i in range(mean_std.shape[1]) :
+    #     print('{}: {} | {}'.format(i, mean_std[0, i], mean_std[1, i]))
+    np.save('mean_std.npy', mean_std)
 
 
 def test():
@@ -23,4 +28,4 @@ def test():
 
 if __name__ == '__main__':
     #save_mean_std(y_train_path, 'y_train_mean_std.npy')
-    save_mean_std(x_train_path, 'x_train_mean_std.npy')
+    save_mean_std()
