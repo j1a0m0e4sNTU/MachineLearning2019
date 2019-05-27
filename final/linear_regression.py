6bb48d2ce3038ba439e34972c2b054e56b9abc2a
James Lin <b04901069@ntu.edu.tw>
2019-05-27 11:47:26 +0800

    add exp

--- final/linear_regression.py
+++ final/linear_regression.py
@@ -1,6 +1,7 @@
 import  numpy as np
 import argparse
 from util import *
+from loss_manager import LossManager
 
 parser = argparse.ArgumentParser()
 parser.add_argument('mode', choices=['train','predict', 'exp'])
@@ -51,15 +52,20 @@ def predict(file_name, regularization):
 
 def expiriment():
     x_train, y_train, x_valid, y_valid = get_train_data(0.2)
+    loss = LossManager()
     feature_size = 10000
     interval = 100
     
     for i in range(feature_size // interval):
         f_start = i * interval
         f_end   = (i + 1) * interval
-        result = train(x_train[f_start:f_end], y_train, x_valid[f_start:f_end], y_valid, args.regular)
-        train_mse, train_wmae, train_nae, valid_mse, valid_wmae, valid_nae = result
-        
+        results = train(x_train[:, f_start:f_end], y_train, x_valid[:, f_start:f_end], y_valid, args.regular)
+        loss.record(f_end, results)
+    
+    loss.plot_mse('MSE - linear regression with different features', 'features', 'loss', 'records/linear-different-feature-mse.png')
+    loss.plot_wmae('WMAE - linear regression with different features', 'features', 'loss', 'records/linear-different-feature-wmae.png')
+    loss.plot_nae('NAE - linear regression with different features', 'features', 'loss', 'records/linear-different-feature-nae.png')
+
 if __name__ == '__main__':
     if args.mode == 'train':
         print('- TRAIN -')
