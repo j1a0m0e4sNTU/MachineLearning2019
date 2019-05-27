feab8a4118e3023418839f75c289242d508d0681
j1a0m0e4s <b04901069@ntu.edu.tw>
2019-05-27 23:19:16 +0800

    fix

--- final/gradient_boost.py
+++ final/gradient_boost.py
@@ -12,18 +12,21 @@ def train(steps, regular, x_train, y_train, x_valid, y_valid):
     pred_valid = np.zeros_like(y_valid)
 
     for step in range(steps):
+        alphas = []
         for i in range(3):
             diff = y_train[:, i] - pred_train[:, i]
             weight = linear_regression(x_train, diff, regular)
-            alpha  = linear_regression(x_train @ weight, diff, 0)
+            pred = (x_train @ weight).reshape((-1, 1))
+            alpha  = linear_regression(pred, diff, 0)
             pred_train[:, i] += (x_train @ weight) * alpha
             pred_valid[:, i] += (x_valid @ weight) * alpha
-
+            alphas.append(alpha)
+        print(alphas)
         loss.evaluate_and_record(step, pred_train, y_train, pred_valid, y_valid)
     
     print('train WMAE: {}'.format(loss.get_train_wmae()))
     print('valid WMAE: {}'.format(loss.get_valid_wmae()))
-    title = 'Redularization: {} Step: {}'.format(args.regular, args.step)
+    title = 'Redularization: {} Step: {}'.format(regular, steps)
     loss.plot_all(title, 'step', 'gradient-boost-test.png')
 
 def test():
@@ -33,7 +36,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('mode', choices= ['train', 'predict'])
     parser.add_argument('-steps', help= 'How many hypothesis is to be learned', type= int, default= 10)
-    parser.add_argument('-regular', help= 'Regularization weight for linear regression', type= float, defualt= 0.0)
+    parser.add_argument('-regular', help= 'Regularization weight for linear regression', type= float, default= 1)
     parser.add_argument('-csv', help= 'Prediction csv path')
     args = parser.parse_args()
 
