4b87f7f8c12576723b414800944657181e5161fe
James Lin <b04901069@ntu.edu.tw>
2019-06-08 14:17:19 +0800

    add some function in util.py add xgboost.py

--- final/util.py
+++ final/util.py
@@ -10,6 +10,8 @@ y_train_path = '../../data_CSIE_ML/Y_train/arr_0.npy'
 x_test_path  = '../../data_CSIE_ML/X_test/arr_0.npy'
 x_mean_std_path = 'x_mean_std.npy'
 y_mean_std_path = 'y_mean_std.npy'
+x_sample_path = '../../data_CSIE_ML/X_train/x_sample.npy'
+y_sample_path = '../../data_CSIE_ML/Y_train/y_sample.npy'
 FEATURE_NUM = 3
 
 def normalize(array, mean, std):
@@ -18,7 +20,12 @@ def normalize(array, mean, std):
 def denormalize(array, mean, std):
     return (array * std) + mean
 
-def get_train_data(validation= 0.2):
+def get_train_data(validation= 0.2, 
+                    x_train_path= x_train_path,
+                    y_train_path= y_train_path,
+                    x_mean_std_path= x_mean_std_path,
+                    y_mean_std_path= y_mean_std_path):
+
     x_train_all = np.load(x_train_path)
     x_mean_std = np.load(x_mean_std_path)
     y_train_all = np.load(y_train_path)
@@ -34,6 +41,26 @@ def get_train_data(validation= 0.2):
         return x_train, y_train, x_valid, y_valid
     else:
         return x_train_all, y_train_all, None, None
+
+def get_sample_data(validation= 0.2, 
+                    x_sample_path= x_sample_path,
+                    y_sample_path= y_sample_path,
+                    x_mean_std_path= x_mean_std_path,
+                    y_mean_std_path= y_mean_std_path):
+
+    x_train_all = np.load(x_sample_path)
+    x_mean_std = np.load(x_mean_std_path)
+    x_mean, x_std = x_mean_std[0, :200], x_mean_std[1, :200]
+    y_train_all = np.load(y_sample_path)
+    y_mean_std = np.load(y_mean_std_path)
+    
+    x_train_all = normalize(x_train_all, x_mean, x_std)
+    y_train_all = normalize(y_train_all, y_mean_std[0], y_mean_std[1])
+    
+    cut_size = int((1 - validation) * x_train_all.shape[0])
+    x_train, x_valid = x_train_all[:cut_size], x_train_all[cut_size:]
+    y_train, y_valid = y_train_all[:cut_size], y_train_all[cut_size:]
+    return x_train, y_train, x_valid, y_valid 
 
 def get_test_data():
     x_test_all = np.load(x_test_path)
--- final/xgboost.py
+++ final/xgboost.py
@@ -2,17 +2,15 @@ import argparse
 import numpy as np
 import xgboost 
 from util import *
-def train(params, train_x, train_y, valid_x, valid_y):
+
+def train(model, train_x, train_y, valid_x, valid_y):
     pred_train = np.zeros_like(train_y)
     pred_valid = np.zeros_like(valid_y)
-    valid_matrix = xgboost.DMatrix(valid_x)
-    train_matrix = xgboost.DMatrix(train_x)
 
     for i in range(3):
-        matrix = xgboost.DMatrix(train_x, train_y[:, i])
-        model = xgboost.train(params, matrix)
-        pred_train[:, i] = model.predict(train_matrix)
-        pred_valid[:, i] = model.predict(valid_matrix)
+        model.fit(train_x, train_y)
+        pred_train[:, i] = model.predict(train_x)
+        pred_valid[:, i] = model.predict(valid_y)
         
     train_wmae, train_nae = evaluate(pred_train, train_y)
     valid_wmae, valid_nae = evaluate(pred_valid, valid_y)
@@ -20,11 +18,11 @@ def train(params, train_x, train_y, valid_x, valid_y):
     print('Validation || WMAE: {} NAE: {}'.format(valid_wmae, valid_nae))
     
 def main():
-    train_x, train_y, valid_x, valid_y = get_train_data(0.2)
-    train_x, valid_x = train_x[:, :200], valid_x[:, :200]
+    train_x, train_y, valid_x, valid_y = get_sample_data()
+    # train_x, valid_x = train_x[:, :200], valid_x[:, :200]
 
-    params = {'objective':'reg:squarederror'}
-    train(params, train_x, train_y, valid_x, valid_y)
+    model = xgboost.sklearn.XGBRegressor()
+    train(model, train_x, train_y, valid_x, valid_y)
 
 if __name__ == '__main__':
     main()
