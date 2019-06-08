504948956acf8c8638da47e7404597f408633fab
James Lin <b04901069@ntu.edu.tw>
2019-06-08 22:24:30 +0800

    improve record method

--- final/manager.py
+++ final/manager.py
@@ -11,12 +11,12 @@ class Manager():
             model.load_state_dict(torch.load(args.load))
         self.model = model.to(self.device)
         self.optimizer = optim.Adam(self.model.parameters(), lr= args.lr)
-        self.metric = nn.MSELoss()
+        self.loss_func = nn.MSELoss()
         self.epoch_num = args.epoch
         self.batch_size = args.bs
         self.save = args.save
         self.csv = args.csv
-        self.best = {'epoch': 0, 'wmae': 9999}
+        self.best = {'loss': 9999, 'wmae': 9999, 'nae': 9999}
         
         if args.record:
             self.record_file = open(args.record, 'w')
@@ -31,54 +31,65 @@ class Manager():
     def train(self, train_data, valid_data):
         for epoch in range(self.epoch_num):
             self.model.train()
+            train_loss = 0
             train_wmae, train_nae = 0, 0
             for step, (train_x, train_y) in enumerate(train_data):
                 train_x = train_x.to(self.device)
                 train_y = train_y.to(self.device)
                 out = self.model(train_x)
                 self.optimizer.zero_grad()
-                loss = self.metric(out, train_y)
+                loss = self.loss_func(out, train_y)
                 loss.backward()
                 self.optimizer.step()
-                
+                train_loss += loss.item()
+
                 out = out.detach().cpu().numpy()
                 train_y = train_y.detach().cpu().numpy()
                 wmae, nae = evaluate(out, train_y)
                 train_wmae += wmae 
                 train_nae += nae
             
+            train_loss /= (step + 1)
             train_wmae /= (step + 1)
             train_nae /= (step + 1)
-            valid_wmae, valid_nae = self.validate(valid_data)
+            valid_loss, valid_wmae, valid_nae = self.validate(valid_data)
 
             best_info = ''
+            if valid_loss < self.best['loss']:
+                self.best['loss'] = valid_loss
+                best_info += 'Loss'
             if valid_wmae < self.best['wmae']:
-                self.best['epoch'] = epoch
                 self.best['wmae'] = valid_wmae
-                best_info = '* Best WMAE *'
+                best_info += 'WMAE '
+            if valid_nae < self.best['nae']:
+                self.best['nae'] = valid_nae
+                best_info += 'NAE'
 
-            info = 'Epoch {} | Train WMAE: {} Train NAE: {} Valid WMAE: {} Valid NAE: {}  {}'.format(
-                epoch, train_wmae, train_nae, valid_wmae, valid_nae, best_info
-            )
-            self.record(info)
+            self.record('------------  Epoch {} ----------- Best: {}'.format(epoch, best_info))
+            self.record('Train => Loss: {:.5f} | WMAE: {:.5f} | NAE: {:.5f}'.format(train_loss, train_wmae, train_nae))
+            self.record('Valid => Loss: {:.5f} | WMAE: {:.5f} | NAE: {:.5f}'.format(valid_loss, valid_wmae, valid_nae))
                 
     def validate(self, valid_data):
         self.model.eval()
+        valid_loss = 0
         valid_wmae, valid_nae = 0, 0
         for step, (valid_x, valid_y) in enumerate(valid_data):
             valid_x = valid_x.to(self.device)
             valid_y = valid_y.to(self.device)
             out = self.model(valid_x)
-            
+            loss = self.loss_func(out, valid_y)
+            valid_loss += loss.item()
+
             out = out.detach().cpu().numpy()
             valid_y = valid_y.detach().cpu().numpy()
             wmae, nae = evaluate(out, valid_y)
             valid_wmae += wmae
             valid_nae  += nae
         
+        valid_loss /= (step + 1)
         valid_wmae /= (step + 1)
         valid_nae /= (step + 1)
-        return valid_wmae, valid_nae
+        return valid_loss, valid_wmae, valid_nae
 
     def predict(self, test_data):
         self.model.eval()
