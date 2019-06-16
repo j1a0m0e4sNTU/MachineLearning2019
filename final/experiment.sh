7694c2317996a2f4bd28a095b3a99ad589043118
j1a0m0e4s <b04901069@ntu.edu.tw>
2019-06-16 21:12:04 +0800

    finish pred for wmae with rnn_I

--- final/experiment.sh
+++ final/experiment.sh
@@ -27,5 +27,7 @@
 # python3 main.py train -input_dim 20 -model E -bs 125 -info "0615 | RNN (E) | first 200 feature + quadratic | ABSLoss" -record records/0616_rnn_E.txt
 # python3 main.py train -input_dim 20 -model F -bs 125 -info "0615 | RNN (F) | first 200 feature + quadratic | ABSLoss" -record records/0616_rnn_F.txt
 # python3 main.py train -input_dim 20 -model G -bs 125 -info "0615 | RNN (G) | first 200 feature + quadratic | ABSLoss" -record records/0616_rnn_G.txt
-python3 main.py train -input_dim 20 -model H -bs 125 -info "0615 | RNN (H) | first 200 feature + quadratic | ABSLoss" -record records/0616_rnn_H.txt
-python3 main.py train -input_dim 20 -model I -bs 125 -info "0615 | RNN (I) | first 200 feature + quadratic | ABSLoss" -record records/0616_rnn_I.txt
+# python3 main.py train -input_dim 20 -model H -bs 125 -info "0615 | RNN (H) | first 200 feature + quadratic | ABSLoss" -record records/0616_rnn_H.txt
+python3 main.py train -input_dim 20 -model I -bs 125 -info "0615 | RNN (I) | first 200 feature + quadratic | ABSLoss" -record records/0616_rnn_I.txt -save ../../weights/0616_rnn_I.pkl
+python3 main.py predict -input_dim 20 -model I -bs 125 -load ../../weights/0616_rnn_I.pkl -csv prediction/0616_rnn_I.csv
+
