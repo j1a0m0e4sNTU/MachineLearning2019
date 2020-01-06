0304ef5a5b88adfbc6464109881b8883bbda83f5
James@VLL <j1a0m0e4s@gmail.com>
2020-01-06 11:44:51 +0800

    code for ploting figure

--- MachineLearningFoundation2019Fall/hw3/hw3.py
+++ MachineLearningFoundation2019Fall/hw3/hw3.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+from matplotlib import pyplot as plt
 
 def load_data(path):
     data = np.genfromtxt(path)
@@ -54,6 +55,53 @@ def stochastic_gd():
     print("E_in : {}".format(error(train_x, train_y, w)))
     print("E_out: {}".format(error(test_x, test_y, w)))
 
+def p78():
+    train_x, train_y = load_data("train.dat")
+    test_x, test_y = load_data("test.dat")
+    eta = 0.01
+    T = 2000
+    gd_ein = []
+    gd_eout = []
+    sgd_ein = []
+    sgd_eout = []
+
+    w = np.zeros((train_x.shape[1]))
+    for _ in range(T):
+        theta = (-1) * train_y * (train_x @ w)
+        theta = 1 / (1 + np.exp(-theta))
+        N = train_x.shape[0]
+        yx = (-1) * train_x * train_y.reshape(-1, 1)
+        grad = (1/N) * (yx.T @ theta)
+        w -= eta * grad
+        gd_ein.append(error(train_x, train_y, w))
+        gd_eout.append(error(test_x, test_y, w))
+
+    w = np.zeros((train_x.shape[1]))
+    for t in range(T):
+        N = train_x.shape[0]
+        idx = t % N
+        x = train_x[idx]
+        y = train_y[idx]
+        theta = (-1) * y * (x @ w)
+        theta = 1 / (1 + np.exp(-theta))
+        yx = (-1) * x * y
+        grad = yx * theta
+        w -= eta * grad
+        sgd_ein.append(error(train_x, train_y ,w))
+        sgd_eout.append(error(test_x, test_y, w))
+    
+    plt.title("E in")
+    plt.plot(gd_ein, c= "r")
+    plt.plot(sgd_ein, c= "b")
+    plt.savefig("images/p7.png")
+    plt.close()
+
+    plt.title("E out")
+    plt.plot(gd_eout, c= "r")
+    plt.plot(sgd_eout, c= "b")
+    plt.savefig("images/p8.png")
+    plt.close()
+
 
 if __name__ == "__main__":
-    stochastic_gd()
+    p78()
